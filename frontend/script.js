5d8c34a26e3892e7030094476f59e1281472a91b
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-02-18 19:04:24 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,18 @@
 const clickit=document.getElementById('click-it').onclick=()=>{
 const username=document.getElementById('username').value
 const password=document.getElementById('password').value
+const one=document.getElementById('header').innerHTML="is it going to server"
 
-fetch('/get',{
+fetch('/data',{
 method:'POST',
 headers:{
-'Content-Type': 'application/json'
+'Content-Type':'application/json'
 },
 body:JSON.stringify({
-username, password
+username, password, one
 })
-}).then(res=>res.json()).then(get=>{
-console.log(get)
+}).then(res => res.json()).then(data =>{
+console.log(data)
 })
 }
 
