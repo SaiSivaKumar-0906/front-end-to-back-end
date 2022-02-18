7ef825309e7b14bfd03303275d558b83005a924d
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-02-18 19:03:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express=require('express')
 const one=express()
-const port=1042
+const port=process.env.PORT||1042
 const path=require('path')
-const body=require('body-parser')
 
 
-one.use('/frontend',express.static(path.resolve(__dirname,'frontend')))
-one.use(body.json())
+one.use('/frontend',express.static('frontend'))
+one.use(express.json())
 
-one.post('/get',(req, res)=>{
-console.log(req)
-res.json({statuc:'ok'})
+one.post('/data', (req, res)=>{
+console.log(req.body)
 })
 
 
